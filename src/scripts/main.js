13ac49c9fdb1cb3ca56963323f3c08a23433ac1c
Denis Pitsul <denia.pitsul@gmail.com>
2025-05-04 14:50:18 +0300

    fix ai mentor comment

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -5,7 +5,9 @@ const tableRowElements = document.querySelectorAll('table tr');
 [...tableRowElements].forEach((rowElement) => {
   const columnElements = rowElement.children;
 
-  const copySecondColumnElement = columnElements[1].cloneNode(true);
+  if (columnElements.length > 1) {
+    const copySecondColumnElement = columnElements[1].cloneNode(true);
 
-  columnElements[columnElements.length - 1].before(copySecondColumnElement);
+    columnElements[columnElements.length - 1].before(copySecondColumnElement);
+  }
 });
