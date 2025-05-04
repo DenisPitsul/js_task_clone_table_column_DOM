23782247f018007436d60430815f3924ac1fd9dc
Denis Pitsul <denia.pitsul@gmail.com>
2025-05-04 14:51:04 +0300

    make build

---
+++ index.f75de5e1.js
@@ -0,0 +1,13 @@
+"use strict";
+const tableRowElements = document.querySelectorAll("table tr");
+[
+    ...tableRowElements
+].forEach((rowElement)=>{
+    const columnElements = rowElement.children;
+    if (columnElements.length > 1) {
+        const copySecondColumnElement = columnElements[1].cloneNode(true);
+        columnElements[columnElements.length - 1].before(copySecondColumnElement);
+    }
+});
+
+//# sourceMappingURL=index.f75de5e1.js.map
